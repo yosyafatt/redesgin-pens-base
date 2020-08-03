b22de1d057daabf1e60662e1f2a7cfd2fd077ae0
Yosyafat Aulia Ferdiansyah <alferdiansyah@it.student.pens.ac.id>
2020-08-04 01:17:52 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    require_once 'vendor/autoload.php';
+    
+    $loader = new \Twig\Loader\FilesystemLoader('templates');
+    $twig = new \Twig\Environment($loader);
+
+    echo $twig->render('index.html', array(
+        'nama' => 'Yosyafattt',
+    ));
+ ?>
